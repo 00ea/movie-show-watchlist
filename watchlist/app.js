68ad77fc252d4fae17d1a7be2e707d1a614c4092
00ea <excelaibangbee@gmail.com>
2025-11-18 14:12:42 -0500

    Add EJS layout support and watchlist views
    - added express-ejs-layouts for EJS layout support
    - added new pages for home, catalog, add item, item added, and partials for header and footer
    - added models for movies and shows
    - updated routes to support new pages and item addition
    - included new images for branding.
    - removed the old index.ejs view and updated dependencies.

--- watchlist/app.js
+++ watchlist/app.js
@@ -4,9 +4,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +21,8 @@ mongoose.connect(mongoURI)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
 
 app.use(logger('dev'));
 app.use(express.json());
--- watchlist/routes/index.js
+++ watchlist/routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Home' });
 });
+
+/* GET catalog page. */
+router.get('/catalog', function(req, res, next) {
+  res.render('catalog', { title: 'Catalog' });
+});
+
+/* GET additem page. */
+router.get('/additem', function(req, res, next) {
+  res.render('additem', { title: 'Add Item' });
+});
+
+router.post('/additem', async function(req, res) {
+  await Book.create(req.body);
+  res.redirect('/itemadded');
+});
+
+// Book Added
+router.get('/itemadded', function(req, res) {
+  res.render('itemadded', { title: 'Success!' });
+});
+
 
 module.exports = router;
